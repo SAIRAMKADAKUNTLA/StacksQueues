2316585a2ac57b8ef25865481f836b8098a26ff2
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-15 20:58:58 +0530

    [Sairam]Added:adding the elements into the list

---
+++ Stacks___Queues/Stacks___Queues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stacks___Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to stack and queue problems");
+            StackList list = new StackList();
+            list.Push(56);
+            list.Push(30);
+            list.Push(70);
+            Console.WriteLine("elements are");
+            list.Show();
+        }
+    }
+}
---
+++ Stacks___Queues/Stacks___Queues/StackList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks___Queues
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+    }
+    public class StackList
+    {
+        public Node top;
+        public StackList()
+        {
+            this.top = null;
+        }
+        public bool isEmpty()
+        {
+            if (this.top == null)
+                return true;
+            else
+                return false;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            if(isEmpty() == true)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("elements are"+node.data);
+        }
+        public void Show()
+        {
+            Node node = this.top;
+            while(node != null)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+        }
+    }
+}
